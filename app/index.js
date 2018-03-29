5cf71a4f2bbae2466e6bf8e44a85269a730fc0fb
Paul Joe George(JP_SMN) <31526130+paul-ml@users.noreply.github.com>
2018-03-29 15:39:56 +0900

    refs #push the basic code

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const express    = require('express');
+const Blockchain = require('../blockchain');
+
+const HTTP_PORT = process.env.HTTP_PORT||3001;			// while running we can use HTTP_PORT:3000 npm run dev
+const app = express();
+const bc = new Blockchain();
+
+app.get('/block', (req, res) => {
+	res.json(bc.chain);
+});
+
+app.listen(HTTP_PORT, () => console.log(`listening to port ${HTTP_PORT}`));
